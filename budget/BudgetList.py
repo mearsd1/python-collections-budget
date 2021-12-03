0d7c9e726284248ae9530eca127ac133b0e7d691
Dillon <dillonfmears@gmail.com>
2021-12-03 10:27:34 -0500

    Finish Making BudgetList an Iterable

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,4 @@
+from _typeshed import Self
 from . import Expense
 
 class BudgetList:
@@ -19,6 +20,18 @@ class BudgetList:
     def __len__(self):
         return len(self.expenses) + len(self.overages)
 
+    def __inter__(self):
+        self.iter_e = iter(self.expenses)
+        self.iter_o = iter(self.overages)
+        return self
+
+    def __next__(self):
+        try:
+            return self.iter_e__next__()
+        except StopIteration as stop:
+            return self.iter_o.__next__()
+
+
 def main():
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
@@ -27,6 +40,9 @@ def main():
         myBudgetList.append(expense.amount)
 
     print('The count of all expenses: ' + str(len(myBudgetList)))
+
+    for entry in myBudgetList:
+        print(entry)
 
 if __name__ == "__main__":
     main()
